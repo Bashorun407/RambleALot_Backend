de10f4b262ebf80d28c61560eb1cb77f2aee435d
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-09-09 13:43:20 +0100

    Created Security package with security configuratin classes

--- src/main/java/com/ramblealot/localsearch/security/CustomUserDetailsService.java
+++ src/main/java/com/ramblealot/localsearch/security/CustomUserDetailsService.java
@@ -1,4 +1,30 @@
 package com.ramblealot.localsearch.security;
 
-public class CustomUserDetailsService {
+import com.ramblealot.localsearch.model.User;
+import com.ramblealot.localsearch.service.UserService;
+import jdk.jfr.Registered;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findEntityByEmail(username);
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
 }
